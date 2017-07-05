0f1d207236fca57af182a76fe024eabf8c587234
Lance Kuo <lancekuo@gmail.com>
2017-07-05 16:15:36 -0400

    Remove tag for docker-compose and docker-machine

--- instance-bastion.tf
+++ instance-bastion.tf
@@ -27,8 +27,6 @@ resource "aws_instance" "swarm-bastion" {
         Project        = "${var.project}"
         Role           = "bastion"
         Index          = "${count.index}"
-        Docker-machine = "sudo curl -L https://github.com/docker/machine/releases/download/v0.10.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine"
-        Docker-compose = "sudo curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose"
     }
     user_data  = "${element(data.template_file.user-data-bastion.*.rendered, count.index)}"
 }
