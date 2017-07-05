b2ebf86fb04a7dac67f194c33505277bf4fa8da2
Lance Kuo <lancekuo@gmail.com>
2017-07-05 16:04:17 -0400

    Remove inline command for installing docker-machine and docker-composer

--- instance-bastion.tf
+++ instance-bastion.tf
@@ -21,17 +21,6 @@ resource "aws_instance" "swarm-bastion" {
     vpc_security_group_ids = ["${aws_security_group.swarm-bastion.id}"]
     subnet_id              = "${element(split(",", var.subnet_public), var.subnet_on_public)}"
 
-    connection {
-        type        = "ssh"
-        user        = "ubuntu"
-        private_key = "${file("${path.root}${var.bastion_private_key_path}")}"
-    }
-    provisioner "remote-exec" {
-        inline = [
-            "sudo curl -L https://github.com/docker/machine/releases/download/v0.10.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine",
-            "sudo curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose",
-        ]
-    }
     tags  {
         Name           = "${terraform.env}-${lower(var.project)}-bastion-${count.index}"
         Env            = "${terraform.env}"
