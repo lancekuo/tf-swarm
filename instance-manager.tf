23200803022c8330a97dc18a6f7d4718f75f0ca1
Lance Kuo <lancekuo@gmail.com>
2018-01-11 15:33:34 -0500

    Auto Manager/Worker join/demote/drain/leave from Swarm gracefully.
    Pull Prometheus and ELK on Manager node.

--- instance-manager.tf
+++ instance-manager.tf
@@ -45,8 +45,43 @@ resource "aws_instance" "swarm-manager" {
     }
 
     provisioner "remote-exec" {
-        inline = [" if [ ${count.index} -eq 0 ]; then sudo docker swarm init;git clone https://github.com/lancekuo/prometheus.git;git clone https://github.com/lancekuo/elk.git; else sudo docker swarm join ${aws_instance.swarm-manager.0.private_ip}:2377 --token $(docker -H ${aws_instance.swarm-manager.0.private_ip} swarm join-token -q manager); fi"]
+        inline = [
+            " if [ ${count.index} -eq 0 ]; then sudo docker swarm init; else sudo docker swarm join ${aws_instance.swarm-manager.0.private_ip}:2377 --token $(docker -H ${aws_instance.swarm-manager.0.private_ip} swarm join-token -q manager); fi",
+            " if [ ${count.index} -eq 1 ]; then git clone https://github.com/lancekuo/prometheus.git; fi",
+            " if [ ${count.index} -eq 2 ]; then git clone https://github.com/lancekuo/elk.git; fi"
+        ]
     }
+
+    provisioner "remote-exec" {
+        when = "destroy"
+
+        inline = [
+            "sudo docker node update --availability drain ${self.tags.Name}",
+            "sudo docker node demote ${self.tags.Name}",
+            "sudo docker swarm leave",
+        ]
+        on_failure = "continue"
+    }
+
+    provisioner "remote-exec" {
+        when = "destroy"
+
+        inline = [
+            "sudo docker node rm --force ${self.tags.Name}"
+        ]
+        on_failure = "continue"
+        connection {
+            bastion_host        = "${aws_eip.swarm-bastion.public_ip}"
+            bastion_user        = "ubuntu"
+            bastion_private_key = "${file("${path.root}${var.rsa_key_bastion["private_key_path"]}")}"
+
+            type                = "ssh"
+            user                = "ubuntu"
+            host                = "${aws_instance.swarm-manager.0.private_ip}"
+            private_key         = "${file("${path.root}${var.rsa_key_manager["private_key_path"]}")}"
+        }
+    }
+
     tags  {
         Name    = "${element(data.template_file.hostname-manager.*.rendered, count.index)}"
         Env     = "${terraform.workspace}"
