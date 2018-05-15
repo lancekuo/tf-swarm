1418be25fde585d6a0a26d37f6abd1243eb254ac
Lance Kuo <lance@healthchain.ca>
2018-05-15 18:06:11 -0400

    Update parted command to have best disk performance

--- ebs.tf
+++ ebs.tf
@@ -30,7 +30,7 @@ resource "null_resource" "ebs_trigger" {
     provisioner "remote-exec" {
         inline = [
             "echo '====== Creating Mount point =====';if [ -d ${var.mount_point} ];then echo \"=> The mount endpoint has existed.\";else sudo mkdir ${var.mount_point};sudo chown -R ubuntu:ubuntu ${var.mount_point};sudo chmod 777 ${var.mount_point};echo \"Mount point created.\";fi",
-            "echo '====== Creating Partition =====';if [ ! -b ${var.partition_file} ]; then sudo parted ${var.device_file} --script -- mklabel msdos mkpart primary ext4 0 -1;else echo '=> Partition has created, skipping this step...';fi",
+            "echo '====== Creating Partition =====';if [ ! -b ${var.partition_file} ]; then sudo parted ${var.device_file} --script -- mklabel msdos mkpart primary ext4 0% 100%;else echo '=> Partition has created, skipping this step...';fi",
             "echo '====== Making File system =====';if [ ! \"$(sudo lsblk --fs ${var.partition_file} --nodeps -o FSTYPE -t -n)\" = \"ext4\" ]]; then sudo mkfs.ext4 -F ${var.partition_file};else echo '=> File system has created, skipping this step...';fi",
             "echo '====== Updating fstab file =====';if ! grep -e \"$$(sudo file -s ${var.partition_file}|awk -F\\  '{print $8}')    ${var.mount_point}\" /etc/fstab 1> /dev/null;then echo \"`sudo file -s ${var.partition_file}|awk -F\\  '{print $8}'`    ${var.mount_point}    ext4    defaults,errors=remount-ro    0    0\"| sudo tee -a /etc/fstab;else echo '=> Fstab has updated, no change in this step...'; fi ",
             "echo '====== Mounting Volume =====';if grep -qs '${var.mount_point}' /proc/mounts; then echo \"=> ${var.mount_point} has mounted.\"; else sudo mount `sudo file -s ${var.partition_file}|awk -F\\  '{print $8}'` ${var.mount_point}; fi",
